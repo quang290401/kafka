326589b2e01fc911decf9ddb92c1955858631fa2
quang290401 <quanglm042000@gmail.com>
2024-12-18 11:29:38 +0700

    Update

---
+++ src/main/java/com/example/kafka/service/KafkaProducer.java
@@ -0,0 +1,21 @@
+package com.example.kafka.service;
+
+import com.example.kafka.common.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class KafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    @Autowired
+    private  KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        LOGGER.info(String.format("Message sent -> %s", message));
+        kafkaTemplate.send(AppConstants.TOPIC_NAME, message);
+    }
+}
